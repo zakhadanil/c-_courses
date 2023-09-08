d2a6ead00151d551789c0834b3c0b9eef9fe81ed
zakhadanil <98804556+zakhadanil@users.noreply.github.com>
2023-09-08 14:01:01 +0400

    Add files via upload
    Тестовое задание для ИРЗ на C#

---
+++ testirz_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+namespace testirz_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] list = s.Split(' ');
+            Console.WriteLine($"Параметров: {list.Length}");
+            for (int i=0; i<list.Length;i++)
+            {
+                Console.WriteLine("\t{0,2}[{1,2}]", i, list[i].Length);
+            }            
+        }
+    }
+}
